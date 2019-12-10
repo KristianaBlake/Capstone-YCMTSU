3a9ffaee4956d2bd2548961d7b9caec63ee88c16
Kristiana Blake <kristiana.blake@gmail.com>
2019-12-10 01:26:51 -0600

    created routes with React Router

--- react-capstone/src/App.js
+++ react-capstone/src/App.js
@@ -1,150 +1,51 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import './App.css';
+import UserDashboard from "./UserDashboard"
+import AdminDashboard from "/AdminDashboard"
 import LoginRegisterForm from "./LoginRegisterForm";
 
 
-class App extends React.Component {
-  constructor() {
-    super():
-    this.state = {
-      loggedIn: false,
-      // checking to see if the user is a regular user 
-      loggedInUser: null,
-      // checking to see if user is the administrator
-      isAdministrator: false 
-    };
-  }
+const my404 = () => {
+  return (
+    <div>
+      You Are Lost 
+    </div>
+  )
+}
 
-  // create register route to be passed into the register component
-  register = async registerInfo => {
-  	// we have to fetch this information in the route to our api 
-  	const response = await fect(
-  		process.env.REACT_APP_API_URL + "api/v1/users/register",
-  		{
-  			method: "POST",
-  			credentials: "include",
-  			body: JSON.stringify(registerInfo),
-  			headers: {
-  				"Content-Type": "application/json"
-  			}
-  		}
-  	);
-  	const parsedLoginResponse = await response.json();
-
-  	// if the response if cleared
-  	if (parsedLoginResponse.data.username === "administrator") {
-  		this.setState({
-  			isAdministrator: true,
-  			loggedInUser: this.state.loggedInUser
-  		});
-  	} else {
-  		// if the response is good 
-  		if (response.ok) {
-  			this.setState({
-  				loggedIn: true,
-  				loggedInUser: parsedLoginResponse.data
-  			});
-  		} else {
-  			console.log(parsedLoginResponse);
-  		}
-  	}
-  }
-
-  // create a route to login 
-  login = async loginInfo => {
-  	const response = await fetch(
-  		// fetch the response from the API
-  		process.env.REACT_APP_API_URL + "api/v1/users/login",
-  		{
-  			method: "POST",
-  			credentials: "include",
-  			body: JSON.stringify(loginInfo),
-  			headers: {
-  				"Content-Type": "application/json"
-  			}
-  		}
-  	);
-  	const parsedLoginResponse = await response.json();
-
-  	if (parsedLoginResponse.data.username === "administrator") {
-  		this.setState({
-  			isAdministrator: true, 
-  			loggedInUser: this.state.loggedInUser
-  		});
-  	} else {
-  		// if the reponse is good 
-  		if (parsedLoginResponse.status.code === 200) {
-  			this.setState({
-  				loggedIn: true, 
-  				loggedInUser: parsedLoginResponse.data
-  			});
-  		} else {
-  			console.log(parsedLoginResponse);
-  		}
-  	}
-  };
-
-  administratorLogout = async () => {
-  	const response = await fetch(
-  		process.env.REACT_APP_API_URL + "api/v1/users/logout",
-  		{
-  			method: "GET",
-  			credentials: "include"
-  			body: JSON.stringify(),
-  			headers: {
-  				"Content-Type": "application/json"
-  			}
-  		}
-  	);
-  	const parsedLoginResponse = await response.json();
-
-  	if (parsedLoginResponse.status.code === 200) {
-  		this.setState({
-  			isAdministrator: false
-  		});
-  	} else {
-  		console.log(parsedLoginResponse);
-  	}
-  };
-
-  userLogout = async () => {
-  	const response = await fetch(
-  		process.env/REACT_APP_API_URL + "api/v1/users/logout",
-  		{
-  			method: "GET",
-  			credentials: "include",
-  			body: JSON.stringify(),
-  			headers: {
-  				"Content-Type": "application/json"
-  			}
-  		}
-  	);
-  	const parsedLoginResponse = await response.json();
-
-  	if (parsedLoginResponse.status.code === 200) {
-  		this.setState({
-  			loggedIn: false 
-  		});
-  	} else {
-  		console.log(parsedLoginResponse);
-  	}
-  }
-};
+const App = () => {
+  return (
+    <main>
+      <Switch>
+        <Route exact path="/api/v1/users/register" component={Register} />
+        <Route exact path="api/v1/users/login" component={Login} />
+        <Route exact path="api/v1/submissions/admin" component={AdminDashboard} />
+        <Route exact path="/api/v1/submissions/<submission_id>/update" component={UserDashboard} />
+        <Route exact path="/api/v1/submissions/<submission_id>/delete" component={UserDashboard} />
+        <Route exact path="/api/v1/submissions/<submission_id>/delete" component={UserDashboard} />
+        <Route exact path="/api/v1/submissions/dasboard/<user_id>" component={UserDashboard} />
+        <Route exact path="/api/v1/submissions/" component={SubmitStory} />
+        // admin approves a post 
+        <Route exact path="/api/v1/<submission_id>/deny" component={AdminDashboard} />
+        // admin denies a post 
+        <Route exact path="/api/v1/<submission_id>/deny" component={AdminDashboard} />
+        // shows submission under category - Life 
+        <Route exact path="/api/v1/submissions/<category>" component={Life} />
+        // shows submission under category - Friends 
+        <Route exact path="/api/v1/submissions/<category>" component={Friends} />
+        // shows submission under category - Significant Other 
+        <Route exact path="/api/v1/submissions/<category>" component={Significant Other} />
+        // shows submission under category - Family
+        <Route exact path="/api/v1/submissions/<category>" component={Family} />
+        <Route component={My404} /> 
+      </Switch>
+    </main> 
+  )
+}
 
 render() {
-	const componentToRender = () => {
-		// if they are the administrator, take them to the administrator dashboard 
-		if (this.state.isAdministrator){
-			return (
-				<AdminContainer
-				loggedInUser={this.state.loggedInUser})
-				administratorLogout={this.administratorLogout}
-				/>
-		} 
-
-	};
-
-	return <div className="App">{componentToRender()}</div>;
-}
+	
+};
 
 export default App;
--- react-capstone/src/LoginRegisterForm/index.js
+++ react-capstone/src/LoginRegisterForm/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Form, Button, Label, Header, Image, Divider } from "semantic-ui-react";
 
-class LoginREgisterForm extends Component {
+class LoginRegisterForm extends Component {
 	constructor() {
 		super();
 
@@ -55,6 +55,70 @@ class LoginREgisterForm extends Component {
 	render() {
 		return (
 			<div>
+			 <div>
+	          <Header>
+	            <h1> You Can't Make This Stuff Up </h1>
+	          </Header>
+	        </div>
+	        <div className="LoginRegisterForm">
+	          <Form onSubmit={this.handleSubmit}>
+	            {this.state.action === "register" ? (
+	              <React.Fragment>
+	                <Label>Name:</Label>
+	                <Divider hidden />
+	                <Form.Input
+	                  type="name"
+	                  name="name"
+	                  value={this.state.name}
+	                  onChange={this.handleChange}
+	                ></Form.Input>
+	              </React.Fragment>
+	            ) : null}
+
+	            <Label>Username:</Label>
+	            <Divider hidden />
+	            <Form.Input
+	              type="username"
+	              name="username"
+	              value={this.state.username}
+	              onChange={this.handleChange}
+	            ></Form.Input>
+
+	            <Label>Email:</Label>
+	            <Divider hidden />
+	            <Form.Input
+	              type="email"
+	              name="email"
+	              value={this.state.email}
+	              onChange={this.handleChange}
+	            ></Form.Input>
+
+	            <Label>Password:</Label>
+	            <Divider hidden />
+	            <Form.Input
+	              type="password"
+	              name="password"
+	              value={this.state.password}
+	              onChange={this.handleChange}
+	            ></Form.Input>
+
+	            <Divider hidden />
+	            <Button type="Submit">
+	              {this.state.action === "register" ? "Register" : "Login"}
+	            </Button>
+	          </Form>
+	          {this.state.action === "register" ? (
+	            <small>
+	              Already have an account? Log in{" "}
+	              <span onClick={this.switchForm}>here</span>
+	            </small>
+	          ) : (
+	            <small>
+	              Need an account? Sign up{" "}
+	              <span onClick={this.switchForm}>here</span>!
+	            </small>
+	          )}
+	        </div>
 			</div> 
 		)
 	}
